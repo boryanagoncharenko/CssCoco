6bbf57c5cfcd66ee3dc6b48f3909c39c2770361d
Boryana <boryana.goncharenko@gmail.com>
2015-07-25 19:59:26 +0200

    updated gitignore file to ignore the uild folders; updated main to avoid a bug with sys.args

--- build/lib/csscoco/__init__.py
+++
@@ -1,4 +0,0 @@
-
-
-def main():
-    print('Bla di bla di bla!')
--- build/lib/csscoco/css/index.js
+++
@@ -1,5 +0,0 @@
-var gonzo = require('gonzales-ast');
-var ast = gonzo.parse('a {margin: 0}'); // formerly 
-var css_string = gonzo.toCSS(ast); // formerly 
- 
- console.log(ast)
