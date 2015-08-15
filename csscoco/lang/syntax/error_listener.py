7535fe967228b0c7972b8b5cb59cd41940573db3
Boryana <boryana.goncharenko@gmail.com>
2015-08-15 21:29:19 +0200

    added syntax error listener

---
+++ csscoco/lang/syntax/error_listener.py
@@ -0,0 +1,21 @@
+from antlr4.error.ErrorListener import ErrorListener
+
+
+class CocoErrorListener(ErrorListener):
+    def __init__(self):
+        self.errors = []
+
+    def has_errors(self):
+        return self.errors
+
+    def print_errors(self):
+        if not self.has_errors():
+            return ''
+        res = 'Error log:\n'
+        for e in self.errors:
+            res = ''.join([res, e, '\n'])
+        return res[:-1]
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        error = ''.join(['Error on line ', str(line), ' - ', msg])
+        self.errors.append(error)
--- csscoco/main.py
+++ csscoco/main.py
@@ -8,6 +8,7 @@ import csscoco.css.parse_tree as css
 import csscoco.lang.syntax.ast_builder as ast
 import csscoco.lang.syntax.cocoLexer as cocoLexer
 import csscoco.lang.syntax.cocoParser as cocoParser
+import csscoco.lang.syntax.error_listener as errors
 import csscoco.lang.analysis.violations as violations
 import csscoco.lang.analysis.type_checker as checker
 
@@ -26,15 +27,21 @@ def get_css_parse_tree():
     return a, ''
 
 
-def get_coco_ast():
+def try_get_coco_ast():
     input_stream = antlr4.FileStream(sys.argv[2])
     lexer = cocoLexer.cocoLexer(input_stream)
+
     stream = antlr4.CommonTokenStream(lexer)
     parser = cocoParser.cocoParser(stream)
+    parser.removeParseListeners()
+    error_listener = errors.CocoErrorListener()
+    parser.addErrorListener(error_listener)
     tree = parser.stylesheet()
+    if error_listener.has_errors():
+        return False, error_listener.print_errors()
     visitor = ast.CocoCustomVisitor()
     convention_set = visitor.visitStylesheet(tree)
-    return convention_set
+    return True, convention_set
 
 
 def main():
@@ -43,7 +50,11 @@ def main():
         print('Please check the validity of your css.')
         exit()
 
-    coco_ast = get_coco_ast()
+    successful, output = try_get_coco_ast()
+    if not successful:
+        print(output)
+        exit()
+    coco_ast = output
     errors = checker.TypeChecker.check(coco_ast)
     if errors.contain_errors():
         print(errors.string())
